e0c054c7f116f26894f5e95648856c838c011f9d
L4azy <hungnqse183854@fpt.edu.vn>
2025-11-09 22:32:55 +0700

    Add: SQL script to create AI Assistant user (ID=23) for chat system

---
+++ SalesApp/create_ai_user.sql
@@ -0,0 +1,36 @@
+-- Create AI Assistant User for Chat System
+-- This user will be used as the AI chatbot in the chat system
+-- UserID must be 23 to match the AI_USER_ID constant in ChatMessageService.java
+
+-- First, enable IDENTITY_INSERT to allow manual ID insertion
+SET IDENTITY_INSERT [dbo].[Users] ON;
+GO
+
+-- Insert AI User with ID = 23
+INSERT INTO [dbo].[Users] (
+    [UserID],
+    [Username],
+    [PasswordHash],
+    [Email],
+    [PhoneNumber],
+    [Address],
+    [Role]
+)
+VALUES (
+    23,                                                          -- UserID (must be 23)
+    'ShopMate AI Assistant',                                    -- Username
+    '$2a$10$1u1atWn1nXM/dvb/2/9LeJjNKTPZZos80Jdy',            -- PasswordHash (dummy - AI doesn't login)
+    'ai-assistant@shopmate.com',                                -- Email
+    'N/A',                                                       -- PhoneNumber
+    'Cloud Server',                                              -- Address
+    'AI'                                                         -- Role (matches RoleEnum.AI)
+);
+GO
+
+-- Disable IDENTITY_INSERT after insertion
+SET IDENTITY_INSERT [dbo].[Users] OFF;
+GO
+
+-- Verify the AI user was created
+SELECT * FROM [dbo].[Users] WHERE [UserID] = 23;
+GO
